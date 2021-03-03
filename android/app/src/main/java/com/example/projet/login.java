65520cf63ca5fa55808ae5222a048fbc1eb937e2
alessio19 <elv-alessio.ncr@eduge.ch>
2021-03-03 11:45:04 +0100

    03.03 création page login et s'enrengistrer (ne sera pas utilisé)

---
+++ android/app/src/main/java/com/example/projet/login.java
@@ -0,0 +1,16 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//https://www.youtube.com/watch?v=TwHmrZxiPA8
+public class login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
